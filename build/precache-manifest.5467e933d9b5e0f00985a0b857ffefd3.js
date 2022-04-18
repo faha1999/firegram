920ac38d57e6f11cd03b135b01f97534494db3ba
faha1999 <kafaha1999@gmail.com>
2022-04-18 20:19:20 +0600

    optimized

---
+++ build/precache-manifest.5467e933d9b5e0f00985a0b857ffefd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0016e56c44f45369c50a8e6d5f12b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb3d48a2c0c3220194e",
+    "url": "/static/css/main.9e3ae081.chunk.css"
+  },
+  {
+    "revision": "09cc9ce0a99b689b3100",
+    "url": "/static/js/2.944440e5.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.944440e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb3d48a2c0c3220194e",
+    "url": "/static/js/main.b98d9607.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
